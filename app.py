1777cfa1d122d0300db9bfedec14f9daa9408378
Lulu <luluching1128@gmail.com>
2023-09-06 23:55:44 +0800

    develop week2

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
 from flask import *
 from flask import Flask
-app=Flask(__name__)
+app=Flask(__name__,static_folder='static')
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
 import json
-import mysql.connector
+from mysql.connector import pooling
 
-# 建立資料庫連線
-db = mysql.connector.connect(
-    host='localhost',
-    port='3306',
-    user='root',
-    password='2926',
-    database='taipei_trip'
-)
-cursor = db.cursor(dictionary=True)
+#  創建一個連接池
+cnx_pool = pooling.MySQLConnectionPool(
+    user='root', 
+    password='2926', 
+    host='localhost', 
+    database='taipei_trip', 
+    pool_name='pool_name', 
+    buffered= True, 
+    pool_size=10)
+
+#定義一個函數來執行資料庫操作
+def execute_query(query,values):
+    cnx = cnx_pool.get_connection()
+    cursor = cnx.cursor()
+    result = None  # 初始化 result 為 None
+    try:
+        cursor.execute(query, values)
+        cnx.commit()
+        result = cursor.fetchall()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        cnx.close()
+    return result
 
 
 @app.route("/api/attractions")
@@ -32,52 +48,53 @@ def attractions():
             return jsonify(error_response), 400
         start_index = page * page_size
         end_index = page_size
-        query = "SELECT * FROM attractions ORDER BY id LIMIT %s,%s;"
-        print(start_index,end_index)
-        values = (start_index, end_index)
-        cursor.execute(query, values)
-        mysql_attractions_list = cursor.fetchall()
+        if keyword =="":
+            query = "SELECT * FROM attractions ORDER BY id LIMIT %s,%s;"
+            values = (start_index, end_index)
+            mysql_attractions_list=execute_query(query, values)
+            #判斷下一頁
+            query = "SELECT * FROM attractions ORDER BY id LIMIT %s,%s;"
+            values = (start_index, end_index+1)
+            judge_attractions_list=execute_query(query, values)
+        else:
+            query = "SELECT * FROM attractions WHERE name LIKE %s OR mrt LIKE %s ORDER BY id LIMIT %s,%s;"
+            values = (f"%{keyword}%", f"%{keyword}%",start_index, end_index)
+            mysql_attractions_list=execute_query(query, values)
+    
+            #判斷下一頁
+            query = "SELECT * FROM attractions WHERE name LIKE %s OR mrt LIKE %s ORDER BY id LIMIT %s,%s;"
+            values = (f"%{keyword}%", f"%{keyword}%",0, 13)
+            judge_attractions_list = execute_query(query, values)
+
         attractions_list = []
         for mysql_attractions in mysql_attractions_list:
-            keyword_name = mysql_attractions['name']
-            keyword_mrt = mysql_attractions['mrt']
-            if len(keyword)==0:
-                data = {
-                    'id': mysql_attractions['id'],
-                    'name': mysql_attractions['name'],
-                    'category': mysql_attractions['category'],
-                    'description': mysql_attractions['description'],
-                    'address': mysql_attractions['address'],
-                    'transport': mysql_attractions['transport'],
-                    'mrt': mysql_attractions['mrt'],
-                    'lat': float(mysql_attractions['lat']),
-                    'lng': float(mysql_attractions['lng']),
-                    'images': mysql_attractions['images'].split(',')
+            data = {
+                'id': mysql_attractions[0],
+                'name': mysql_attractions[1],
+                'category': mysql_attractions[2],
+                'description': mysql_attractions[3],
+                'address': mysql_attractions[4],
+                'transport': mysql_attractions[5],
+                'mrt': mysql_attractions[6],
+                'lat': float(mysql_attractions[7]),
+                'lng': float(mysql_attractions[8]),
+                'images': mysql_attractions[9].split(',')
                 }
-                attractions_list.append(data)
-            elif keyword in keyword_name or keyword in keyword_mrt:
-                data = {
-                    'id': mysql_attractions['id'],
-                    'name': mysql_attractions['name'],
-                    'category': mysql_attractions['category'],
-                    'description': mysql_attractions['description'],
-                    'address': mysql_attractions['address'],
-                    'transport': mysql_attractions['transport'],
-                    'mrt': mysql_attractions['mrt'],
-                    'lat': float(mysql_attractions['lat']),
-                    'lng': float(mysql_attractions['lng']),
-                    'images': mysql_attractions['images'].split(',')
-                }
-                attractions_list.append(data)
+            attractions_list.append(data)
+
         if len(attractions_list)==0:
             error_response = {
 				"error": "true",
 				"message": "沒有此景點資料"
 			}
             return jsonify(error_response), 400
-            
+        
+        if len(judge_attractions_list) == 13 : #判斷頁面數量13
+            nextPage=page+1 
+        else:
+            nextPage=None
         final_data = {
-            'nextPage': page+1,
+            'nextPage': nextPage,
             'data': attractions_list
         }
         return Response(json.dumps(final_data,sort_keys=False), mimetype='application/json')
@@ -94,22 +111,21 @@ def attractionID(attractionID):
     try:
         query = "SELECT*FROM attractions WHERE id=%s;"
         values = (attractionID,)
-        cursor.execute(query, values)
-        mysql_id_list = cursor.fetchall()
+        mysql_id_list = execute_query(query, values)
         id_list = []
         if len(mysql_id_list) != 0:
             for id_mysql in mysql_id_list:
                 data = {
-                    'id': id_mysql['id'],
-                    'name': id_mysql['name'],
-                    'category': id_mysql['category'],
-                    'description': id_mysql['description'],
-                    'address': id_mysql['address'],
-                    'transport': id_mysql['transport'],
-                    'mrt': id_mysql['mrt'],
-                    'lat': float(id_mysql['lat']),
-                    'lng': float(id_mysql['lng']),
-                    'images': id_mysql['images'].split(',')
+                    'id': id_mysql[0],
+                    'name': id_mysql[1],
+                    'category': id_mysql[2],
+                    'description': id_mysql[3],
+                    'address': id_mysql[4],
+                    'transport': id_mysql[5],
+                    'mrt': id_mysql[6],
+                    'lat': float(id_mysql[7]),
+                    'lng': float(id_mysql[8]),
+                    'images': id_mysql[9].split(',')
                 }
                 id_list.append(data)
         else:
@@ -136,12 +152,13 @@ def attractionID(attractionID):
 def mrts():
     try:
         query = "SELECT mrt, COUNT(*) AS station_count FROM attractions GROUP BY mrt ORDER BY station_count DESC"
-        cursor.execute(query)
-        mysql_mrts_list = cursor.fetchall()
+        values=()
+        mysql_mrts_list = execute_query(query,values)
         mrts_list = []
         for mrts_mysql in mysql_mrts_list:
-            data = mrts_mysql['mrt']
-            mrts_list.append(data)
+            data = mrts_mysql[0]
+            if data:
+                mrts_list.append(data)
         final_data = {
             'data': mrts_list
         }
@@ -153,11 +170,12 @@ def mrts():
         }
         return jsonify(error_response), 500
 
-
 # Pages
 @app.route("/")
 def index():
+
 	return render_template("index.html")
+
 @app.route("/attraction/<id>")
 def attraction(id):
 	return render_template("attraction.html")
